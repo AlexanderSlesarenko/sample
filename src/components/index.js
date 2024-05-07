f4d2946f487a3ea1538f5435e8928e050581e61d
AlexanderSlesarenko <slesarenkoa@yandex.ru>
2024-05-07 16:31:11 +0700

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
+// if export this way you get incorrect bundle output:
 export const lazyComp = () => {
   console.log("check lazyComp");
 };
@@ -5,5 +6,7 @@ export const directComp = () => {
   console.log("check directComp");
 };
 
+
+// if you export this way you get proper bundle output:
 // export { default as directComp } from "./directComp";
 // export { default as lazyComp } from "./lazyComp";
